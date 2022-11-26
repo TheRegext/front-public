7431aed0fd60fb3c948d2f2263faf0f217f8bc1c
Brian Lara <brian.lara@outlook.com.ar>
2022-11-26 16:57:19 -0300

    Update product.services.js

--- src/services/product.services.js
+++ src/services/product.services.js
@@ -38,11 +38,19 @@ async function create(product) {
     return fetch('https://back-public.vercel.app/api/productos', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'auth-token': localStorage.getItem('token')
         },
         body: JSON.stringify(product)
     })
-        .then(response => response.json())
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            }
+            else {
+                throw new Error('No se pudo obtener los productos')
+            }
+        })
 }
 
 export {
